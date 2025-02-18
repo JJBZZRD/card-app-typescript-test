085c80932da92f3d7f12c26c432eebcbf0b2d7ea
JJBZZRD <buzzard.jj@gmail.com>
2025-02-18 17:07:46 +0000

    Modified modal to include darkmode setting

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,25 +1,31 @@
-import {NavLink} from 'react-router-dom'
-import { useState } from 'react';
-import SettingsModal from './SettingsModal';
-export default function NavBar(){
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import SettingsModal from "./SettingsModal";
+export default function NavBar() {
   const [isSettingsOpen, setIsSettingsOpen] = useState(false);
   const openSettings = () => setIsSettingsOpen(true);
   const closeSettings = () => setIsSettingsOpen(false);
 
-    return(
-      <>
+  return (
+    <>
       <nav className="flex justify-center gap-5">
-        <NavLink className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white" to={'/'}>All Entries</NavLink>
-        <NavLink className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white" to={'/create'}>New Entry</NavLink>
-        <button 
+        <NavLink className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white" to={"/"}>
+          All Entries
+        </NavLink>
+        <NavLink
+          className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white"
+          to={"/create"}
+        >
+          New Entry
+        </NavLink>
+        <button
           onClick={openSettings}
           className="m-3 p-4 text-xl bg-blue-400 hover:bg-blue-500 rounded-md font-medium text-white"
-          > 
+        >
           Settings
-          </button>
+        </button>
       </nav>
       <SettingsModal isOpen={isSettingsOpen} onClose={closeSettings} />
-      </>
-
-    )
+    </>
+  );
 }
