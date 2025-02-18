cf5c1f9669e310b987957bc303897fb74749b3cf
JJBZZRD <buzzard.jj@gmail.com>
2025-02-18 16:24:49 +0000

    Added hook for controlling root theme mode

---
+++ frontend/src/hooks/useDarkMode.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+
+export default function useDarkMode() {
+    const [darkMode, setDarkMode] = useState<boolean>(() => {
+        const storedMode = localStorage.getItem("darkMode");
+        return storedMode === "true";
+    });
+
+    useEffect(() => {
+        const root = window.document.documentElement;
+
+        if (darkMode) {
+            root.classList.add("dark");
+        } else {
+            root.classList.remove("dark");
+        }
+        localStorage.setItem("darkMode", darkMode.toString());
+    }, [darkMode]);
+
+    return [darkMode, setDarkMode] as const;
+}
