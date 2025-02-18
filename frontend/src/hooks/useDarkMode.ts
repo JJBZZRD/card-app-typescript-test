dfeae30ab0f5823383adb6f0519050595b2fe33d
JJBZZRD <buzzard.jj@gmail.com>
2025-02-18 16:50:55 +0000

    Added setings button and modified headlessui modal

--- frontend/src/hooks/useDarkMode.ts
+++ frontend/src/hooks/useDarkMode.ts
@@ -1,22 +1,21 @@
-import { useState, useEffect } from "react";
-
+import { useEffect, useState } from "react";
 
 export default function useDarkMode() {
-    const [darkMode, setDarkMode] = useState<boolean>(() => {
-        const storedMode = localStorage.getItem("darkMode");
-        return storedMode === "true";
-    });
+  const [darkMode, setDarkMode] = useState<boolean>(() => {
+    const storedMode = localStorage.getItem("darkMode");
+    return storedMode === "true";
+  });
 
-    useEffect(() => {
-        const root = window.document.documentElement;
+  useEffect(() => {
+    const root = window.document.documentElement;
 
-        if (darkMode) {
-            root.classList.add("dark");
-        } else {
-            root.classList.remove("dark");
-        }
-        localStorage.setItem("darkMode", darkMode.toString());
-    }, [darkMode]);
+    if (darkMode) {
+      root.classList.add("dark");
+    } else {
+      root.classList.remove("dark");
+    }
+    localStorage.setItem("darkMode", darkMode.toString());
+  }, [darkMode]);
 
-    return [darkMode, setDarkMode] as const;
+  return [darkMode, setDarkMode] as const;
 }
